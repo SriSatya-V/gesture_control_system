5e640a03d6bcccfe678abedeac84f936fb413d50
SriSatyaVeeravalli <satyaveeravalli111@gmail.com>
2026-02-08 20:07:11 +0530

    Add configuration settings for gesture system

---
+++ config.py
@@ -0,0 +1,6 @@
+HOLD_TIME = 0.5          # Time to hold a static gesture (Volume/Pause)
+SWIPE_THRESHOLD = 40     # How fast hand must move to count as swipe
+COOLDOWN = 1.0           # Time between actions to prevent double clicks
+BRIGHTNESS_STEP = 10
+CAMERA_WIDTH = 640
+CAMERA_HEIGHT = 480
